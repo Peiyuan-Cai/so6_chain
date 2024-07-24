56d0a7dae3e4f9fc4e3cd74f2f2f8e6eef3d5912
Peiyuan-Cai <mailbox_for_cpy@126.com>
2024-07-24 23:44:28 +0800

    240724 latest

--- clustercode/multisubso6.sh
+++ clustercode/multisubso6.sh
@@ -14,8 +14,9 @@ for pbc in 1 0
 do
 for K in 0.0 0.2 0.3 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 1.9 2.0 2.1 2.2 3.8 3.9 4.0 4.1 4.2 7.8 7.9 8.0 8.1 8.2 10.0 12.0 16.0
 do
-echo J = $J K = $K, pbc = ${pbc}, D = ${D}, job = ${job}
-sbatch -J SO6DMRG_lx${lx}_J${J}_K${K}_pbc${pbc}_D${D}_Sweeps${sweeps}_Job${job} subso6.sh ${lx} ${J} ${K} ${D} ${pbc} ${sweeps} ${job}
+echo Job = ${job} J = $J K = $K, pbc = ${pbc}, D = ${D}
+jname=SO6DMRG_Job${job}_lx${lx}_J${J}_K${K}_pbc${pbc}_D${D}_Sweeps${sweeps}
+sbatch -J ${jname} subso6.sh ${lx} ${J} ${K} ${D} ${pbc} ${sweeps} ${job}
 done
 done
 done
--- clustercode/subso6.sh
+++ clustercode/subso6.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=SO6DMRG_lx$1_K$2_pbc$5_D$4_Job&7
 #SBATCH --partition=fat6348
 #SBATCH --ntasks-per-node=2
-#SBATCH --output=out%x_%j.out
+#SBATCH --output=%x_%j.out
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK  # number of CPUs per node, total for all the tasks below.
 
