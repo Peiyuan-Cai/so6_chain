dbc851725833a52c47c65a5c35a25f66cb25be5f
Peiyuan-Cai <mailbox_for_cpy@126.com>
2024-09-20 09:46:37 +0800

    so4dmrg initial commit

---
+++ dmrg/so4/so4dmrg.py
@@ -0,0 +1,19 @@
+"""
+SO4 chain DMRG code compact version for HPC use. As a copy of so6dmrg.py
+
+Puiyuen 240918-
+"""
+import numpy as np
+import numpy.linalg as LA
+from copy import deepcopy
+from tenpy.models.model import CouplingModel, MPOModel
+from tenpy.networks.site import SpinSite, FermionSite, Site
+from tenpy.models.lattice import Chain
+from tenpy.networks.mps import MPS
+from tenpy.networks.mpo import MPO
+import tenpy.linalg.np_conserved as npc
+from tenpy.linalg.charges import LegCharge, ChargeInfo
+from tenpy.algorithms import dmrg
+from tenpy.tools.params import asConfig
+import pickle
+
