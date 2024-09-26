d0fd73052cdc0caa8ace5035de9ed6eb2e4b3a13
Peiyuan-Cai <mailbox_for_cpy@126.com>
2024-09-26 12:40:05 +0800

    change to plot by hand

--- clustercode/engdiffplot.py
+++ clustercode/engdiffplot.py
@@ -3,6 +3,20 @@ import pickle
 from so6bbqham import *
 import matplotlib.pyplot as plt
 
+import numpy as np
+import numpy.linalg as LA
+from copy import deepcopy
+from tenpy.models.model import CouplingModel, MPOModel
+from tenpy.networks.site import SpinSite, FermionSite, Site
+from tenpy.models.lattice import Chain
+from tenpy.networks.mps import MPS
+from tenpy.networks.mpo import MPO
+import tenpy.linalg.np_conserved as npc
+from tenpy.linalg.charges import LegCharge, ChargeInfo
+from tenpy.algorithms import dmrg
+from tenpy.tools.params import asConfig
+import pickle
+
 import os
 homepath  = os.getcwd()
 if os.path.isdir(homepath+'/data/') == False:
